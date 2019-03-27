38ef37f98db26e2533437933e5c21a272e329462
Manuel Grundner <m.grundner@delegate.at>
2019-03-27 12:15:26 +0100

    fix: global assembly info now parseable

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -2,17 +2,12 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else 
-[assembly: AssemblyConfiguration("Release")]
-#endif
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fa. Manuel Grundner")]
 [assembly: AssemblyDescription("This project describes how to pre cache all files for an XAF application")]
 [assembly: AssemblyProduct("how-to-precache-an-xaf-winforms-application.Win")]
 [assembly: AssemblyCopyright("Copyright Manuel Grundner © 2019")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
