3043260935e523e63fd7b3ea9929fe8ae289832e
Manuel Grundner <m.grundner@delegate.at>
2019-03-29 11:55:07 +0100

    feat: added working msbuild task

--- src/Scissors.Xaf.CacheWarmup.Generators.MsBuild/XafCacheWarmupTask.cs
+++ src/Scissors.Xaf.CacheWarmup.Generators.MsBuild/XafCacheWarmupTask.cs
@@ -10,8 +10,25 @@ namespace Scissors.Xaf.CacheWarmup.Generators.MsBuild
     {
         public override bool Execute()
         {
-            Console.WriteLine("Hello World");
-            return true;
+            var finder = new AttributeFinder();
+            var assemblyPath = @"C:\F\github\how-to-precache-an-xaf-winforms-application\src\how_to_precache_an_xaf_winforms_application.Win\bin\Debug\how_to_precache_an_xaf_winforms_application.Win.exe";
+            var foundType = finder.FindAttribute(assemblyPath);
+
+            Console.WriteLine(foundType);
+
+            if (foundType != null)
+            {
+                var cacheGenerator = new CacheWarmupGenerator();
+
+                var cacheResult = cacheGenerator.WarmupCache(assemblyPath, foundType);
+                if (cacheResult != null)
+                {
+                    Console.WriteLine("Done");
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
