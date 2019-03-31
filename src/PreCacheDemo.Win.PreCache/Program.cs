7e1fa98043675350f401a7822d5dcaff79497f03
Manuel Grundner <m.grundner@delegate.at>
2019-03-31 12:39:33 +0200

    refactor: removed namespaces & made internal

--- src/PreCacheDemo.Win.PreCache/Program.cs
+++ src/PreCacheDemo.Win.PreCache/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PreCacheDemo.Win.PreCache
 {
-    public static class Program
+    static class Program
     {
         [STAThread]
-        public static void Main()
+        static void Main()
         {
             PreCacheDemo.Win.Program.Main();
         }
